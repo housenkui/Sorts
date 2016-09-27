063474585d80f281756766fe893a5865bd18a0a8
Daves_Code_Hou <840737320@qq.com>
2016-09-27 20:46:13 +0800

    添加一个target

---
+++ selectSort/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  selectSort
+//
+//  Created by 二哈 on 16/9/27.
+//  Copyright © 2016年 侯森魁. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
